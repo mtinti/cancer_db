cd04f6fc30ce17e89d85f46a7586a62c435a3e22
mtinti <michele.tinti@gmail.com>
2017-06-15 09:55:54 +0100

    add main in get data

--- get_data.py
+++ get_data.py
@@ -8,7 +8,7 @@ import pandas as pd
 import sqlite3 as db
 
 
-con = db.connect('cancer_mutect')
+con = db.connect('cancer_mutect.db')
 table_name = 'TCGA_CHOL_mutect'
 
 
@@ -40,14 +40,38 @@ def test_3():
 
 def test_4():
     c = con.cursor()
-    c.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    c.execute("SELECT name FROM sqlite_master WHERE type='table'")
     tables = c.fetchall()
-    print tables
+    for t in tables:
+        print t[0]
+
+
+def main(gene, con):
+    q_gene = '\"'+gene+'\"'
+    c = con.cursor()
+    c.execute("SELECT name FROM sqlite_master WHERE type='table'")
+    tables = c.fetchall()
+    tables = [n[0] for n in tables]
+    #print tables
+    res_df = []
+    for table_name in tables:
+        query_data = 'SELECT * FROM {tn} WHERE Gene = {g}'.format(tn=table_name, g=q_gene)
+        temp_data = pd.read_sql(query_data, con)
+        temp_data['table_name']=table_name
+        res_df.append(temp_data)        
+    res_df = pd.concat(res_df)
+    print  gene, res_df.shape  
+       
+    
+    
 
 if __name__ == '__main__':
-    test_1()
-    test_2()
-    test_3()
-    test_4()
+    #test_1()
+    #test_2()
+    #test_3()
+    #test_4()
+    main('ENSG00000130669', con)
+    main('ENSG00000137843', con)
+    main('ENSG00000101349', con)    
     con.close() 
 
